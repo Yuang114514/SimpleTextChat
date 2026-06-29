4497dee8e70cb7f72e699bb3faa356e5b0821d63
鬝豭鶬鶬 <achilles.hero.way@outlook.com>
2026-06-29 18:54:21 +0800

    Initial Commit

---
+++ src/client/java/cn/yuang2714/simple_text_chat/client/SimpleTextChatClient.java
@@ -0,0 +1,10 @@
+package cn.yuang2714.simple_text_chat.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleTextChatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
